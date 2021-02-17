1c870be94dbbafbcc5c90e7075ba15b7bf657441
willaparecido <wilson_aparecido2015@outlook.com>
2021-02-17 13:59:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Write your code below this line 👇
+print('what do you print')
+print ('what to print')
+
+print("Day 1 - Python Print Function")
+print("The functon is declared like this:")
+print("print('what to print')")
+
+
+ 
+
+
+
+
+
